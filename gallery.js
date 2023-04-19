eed21b413602106ca857ea8a427d3dff4cee1c2a
SiljeP <u2440913@rts-365.dk>
2023-04-19 09:42:04 +0200

    før første pause

---
+++ gallery.js
@@ -0,0 +1,33 @@
+const gallery = (function () {
+    const GALLERY = document.querySelector(".gallery__container");
+    const FOCUSED_IMAGE = document.createElement("img");
+
+    FOCUSED_IMAGE.classList.add("gallery__focusedImage");
+
+    const THUMBNAILS = document.createElement("div");
+    THUMBNAILS.classList.add("gallery__thumbnails");
+
+
+    function buildThumbnail(image) {
+        const BUTTON = document.createElement("button");
+        BUTTON.addEventListener("click", changeFocus);
+        BUTTON.innerHTML = `<img src = "${image}" alt = "gallereri miniature" class = "gallery__thumbnail">`;
+        BUTTON.classList.add("gallery__button");
+        THUMBNAILS.append(BUTTON);
+    }
+
+    function changeFocus(event){
+        FOCUSED_IMAGE.src = event.target.src;
+    }
+
+
+    function init(images = []) {
+        FOCUSED_IMAGE.src = images[0];
+        images.forEach(buildThumbnail); //putter thumbnails ind i div'en
+        GALLERY.append(FOCUSED_IMAGE, THUMBNAILS);
+    }
+    return {
+        init
+    };
+
+})()
